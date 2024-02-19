179dc1741523e7edc26f42cc64216260bd70f2a9
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-19 17:09:33 +0500

    Add files via upload

---
+++ Q43.ts
@@ -0,0 +1,28 @@
+// Q43: Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+
+
+function show_magicians(magicians: string[]): void {
+    console.log("Magicians:");
+    for (const magician of magicians) {
+      console.log(magician);
+    }
+  }
+  
+  function make_great(magicians: string[]): string[] {
+    const greatMagicians: string[] = [];
+  
+    for (const magician of magicians) {
+      greatMagicians.push(`${magician} the Great`);
+    }
+  
+    return greatMagicians;
+  }
+  
+  const magicianNames: string[] = ["Nawaz Sharif", "Asif ALI Zardari", "Maryam Nawaz", "Bilawal Bhutto"];
+  
+  const greatMagicians = make_great([...magicianNames]); 
+  
+  show_magicians(magicianNames);
+  
+  show_magicians(greatMagicians);
+  
